d83d654d32192d35d805c010c4cc0171ad72bce4
[Vlad Doniga] <[vladdoniga11@gmail.com]>
2023-05-27 23:29:35 +0200

    Rework of the ecs stucture

--- Ray_Caster/Attributes.h
+++ Ray_Caster/Attributes.h
@@ -1,50 +1,21 @@
 #pragma once
+#include<map>
+#include<string>
 #include<SFML/Graphics.hpp>
-struct Attribute
-{
-	//Template empty class, nothing to see here
+
+class ECS_Manager {
+private:
+	std::map<std::string, int> Entities_list;
+public:
+
 };
 
-struct Mov : public Attribute
-{
-	//Structure for an object w/ a position and a basic moveset
-	//Struct variables
-	sf::Vector2i position = { 0, 0 };
-
-	//Struct methods
-	virtual void move(int X, int Y) {
-		this->position.x += X;
-		this->position.y += Y;
-	}
-};
-
-struct Phy : public Mov
-{
-	//Same as mother class but with an advanced moveset
-	//Struct variables
-	sf::Vector2i speed = { 0, 0 };
-	sf::Vector2i acceleration = { 0, 0 };
-
-
-	//Struct methods
-	void move(int X, int Y) override {
-		this->speed.x += X;
-		this->speed.y += Y;
-	}
-
-	void accelerate(int X = 0, int Y = 0) {
-		this->acceleration.x += X;
-		this->acceleration.y += Y;
-		this->acceleration += this->speed;
-	}
-};
-
-struct Spr : public Attribute
-{
-	//Structure to define an object with an appearence
-	sf::CircleShape sprite;
-
-	void assign_sprite(sf::CircleShape spr) {
-		sprite = spr;
+class Position {
+private:
+	
+	sf::Vector2i entity_position;
+public:
+	Position() {
+		entity_position = sf::Vector2i(0, 0);
 	}
 };
